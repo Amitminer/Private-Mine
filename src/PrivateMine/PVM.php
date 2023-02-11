baafe44bd584bb632fbab147950d48a51ddd284e
Amitminer <81667979+Amitminer888@users.noreply.github.com>
2023-02-11 13:28:00 +0530

    Added support for config and confirmation Menu

--- src/PrivateMine/PVM.php
+++ src/PrivateMine/PVM.php
@@ -24,6 +24,10 @@ class Main extends PluginBase implements Listener {
     # onLoading plugin
     public function onEnable() : void{
         $this->getLogger()->info("Private mine enabled!!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml");
+        $this->saveDefaultConfig();
     }
     
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
@@ -47,27 +51,60 @@ class Main extends PluginBase implements Listener {
             }
             switch($data){
                 case 0:
-                    $this->coalmine($player);
+                    $this->ConfirmationMenu($player);
                 break;
             }
         });
         
         # Mines List
         $form->setTitle("§l§cPrivate Mines");
-        $form->addButton("Coal Mine", 0, "textures/items/coal");
+        $form->addButton("DiamondBlock Mine", 0, "textures/blocks/diamond_block");
+        $player->sendForm($form);
+        return $form;
+    }
+    
+  public function ConfirmationMenu($player) {
+        $form = new ModalForm(function (Player $player, bool $data) {
+            switch($data) {
+                case true:
+                    $this->coalmine($player);
+                    break;
+                case false:
+                    $this->pvmUI($player);
+                    break;
+            }
+        });
+        $ReferenceNumber = mt_rand(0,9999);
+        $form->setTitle("§3Diamond Block Mine");
+        $form->setContent("§l§9----- CONFIRM PURCHASE -----
+§aItem: §bDiamond Block Mine
+§cPrice: §e50Million 
+Details: §cfor more Details. please visit discord server and check channel/pvm-preview..
+§aBenefits: §2Unlimited access to diamondblock mine and increased mining capabilities
+
+§aReference Number: §c#$ReferenceNumber
+
+§cBy confirming this purchase, you agree to our terms of service and refund policy.
+
+§aConfirm Purchase (Yes, I want to proceed with this purchase)
+§cCancel (No, I want to go back to the previous menu)
+");
+        $form->setButton1("§l§aYes");
+        $form->setButton2("§l§cNo");
         $player->sendForm($form);
         return $form;
     }
     # coal mine function
   public function coalmine(Player $player){
       $amount = 1000;
-      libEco::reduceMoney($player, $amount, static function(bool $success) use($player): void {
+      $title = $this->config->get("title");
+      libEco::reduceMoney($player, $amount, static function(bool $success) use($player, $title): void {
 	if($success){
-	    $player->sendMessage("§aYou have successfully bought Coalmine! Thanks");
+        $player->sendToastNotification($title , "§aYou have successfully bought DiamondBlock Mine! Thanks");
 	} else{
 		$player->sendMessage("§cIt looks like the money in your account aren't sufficient to make this purchase at this time.");
 	}
-}); # closing function 
+});
 
   }
 }
