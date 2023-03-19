16efbd6fb7d360009a722575371bdd6a27b93e34
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-03-19 21:39:02 +0530

    Update PVMUI.php

--- src/PrivateMine/PVMUI.php
+++ src/PrivateMine/PVMUI.php
@@ -41,38 +41,58 @@ class PVMUI {
         });
         
         # Mines List
-        $form->setTitle("§l§cPrivate Mines");
-        $form->addButton("DiamondBlock Mine", 0, "textures/items/diamond");
-        $player->sendForm($form);
-        return $form;
-    }
+        if($this->config->getNested("PrivateMinesSettings.Mine1.enable") === true){
+            $form->setTitle("§l§cPrivate Mines");
+            $form->addButton($this->config->getNested("PrivateMinesSettings.Mine1.name"), 0, $this->config->getNested("PrivateMinesSettings.Mine1.ui-texture"));
+            $player->sendForm($form);
+            return $form;
+        }
+   }
     
    public function ConfirmationMenu($player) {
        $form = new ModalForm(function (Player $player, bool $data) {
             switch($data) {
                 case true:
-                    $this->coalmine($player);
+                    $this->Mine1($player);
                     break;
                 case false:
-                    $this->pvmUI($player);
+                    $this->tpmine1($player);
                     break;
             }
         });
         $ReferenceNumber = mt_rand(0,9999);
-        $form->setTitle("§3Diamond Block Mine");
+        $form->setTitle($this->config->getNested("PrivateMinesSettings.Mine1.name"));
         $form->setContent($this->config->get("confirm-msg"));
         $form->setButton1("§l§aYes");
-        $form->setButton2("§l§cNo");
+        $form->setButton2("§l§cteleport");
         $player->sendForm($form);
         return $form;
     }
-    # coal mine function
-   public function coalmine(Player $player){
+   public function tpmine1(Player $player){
+       if ($player->hasPermission($this->config->getNested("PrivateMinesSettings.Mine1.permission")) {
+           $player->sendMessage("§a§l[OmniCraft] §cteleporting to privatemine.");
+           $server = Server::getInstance();
+           $playerName = $player->getName();
+           $Mine1 = $this->config->getNested("PrivateMinesSettings.Mine1.World");
+	       $server->getWorldManager()->loadWorld($Mine1);
+	       $world = $server->getWorldManager()->getWorldByName($Mine1);
+	       $player->teleport($world->getSafeSpawn());
+            
+        } else{
+            $player->sendMessage("§cIt looks like you haven't purchased a Private Mine yet. for purchasing pvm do/pvm");
+   }
+   }
+    #mine1 function
+   public function Mine1(Player $player){
        $config = $this->config;
-       libEco::reduceMoney($player, $config->get("Amount"), static function(bool $success) use($player, $config): void {
+       $server = Server::getInstance();
+       $plugin = $server->getPluginManager()->getPlugin("PrivateMine");
+       libEco::reduceMoney($player, $config->getNested("PrivateMinesSettings.Mine1.Price"), static function(bool $success) use($player, $config,$plugin): void {
 	if($success){
-        $player->sendToastNotification($config->get("title") , $config->get("success-msg"));
-        worldwhitelist::executeWWL($player);
+        $successMsg = str_replace('{name}', $config->getNested("PrivateMinesSettings.Mine1.name"), $config->get('success-msg'));
+        $player->sendToastNotification($config->get("title"), $successMsg);
+        $wwl = new worldwhitelist($config);
+        $wwl->executeWWL($player);
         #$wwl();
 	} else{
 		$player->sendMessage($config->get("money-insufficient"));
