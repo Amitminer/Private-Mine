6304f9debf442b90a8761f9ff4c2d6f5e8de873d
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-03-19 20:12:02 +0530

    Update PVM.php

--- src/PrivateMine/PVM.php
+++ src/PrivateMine/PVM.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace PrivateMine;
 
 use PrivateMine\worldwhitelist;
+use ArasakaID\WorldWhitelist\Main;
 use PrivateMine\PVMUI;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class PVM extends PluginBase implements Listener {
@@ -20,13 +22,14 @@ class PVM extends PluginBase implements Listener {
     
     # onEnabling plugin
     public function onEnable() : void{
-        self::$instance = $this;
-        $this->getLogger()->info("Private mine enabled!!");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder() . "config.yml");
-        $this->saveDefaultConfig();
+           self::$instance = $this;
+           $this->getLogger()->info("Private mine enabled!!");
+           $this->getServer()->getPluginManager()->registerEvents($this, $this);
+           $this->saveResource("config.yml");
+           $this->config = new Config($this->getDataFolder() . "config.yml");
+           $this->saveDefaultConfig();
     }
+            
     # executing cmd
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
         $pvmui = new PVMUI($this->config);
@@ -35,7 +38,7 @@ class PVM extends PluginBase implements Listener {
                 if($sender instanceof Player) {
                            $pvmui->pvmUI($sender);
                      } else {
-                             $sender->sendMessage("Please execute this command in-game");
+                             $sender->sendMessage("Please execute this command in-game!");
                               return true;
                      }
             break;
